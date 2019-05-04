9e3a854e15d00df4d047e6d22ccaee906baedd7e
omarASC5 <ocobas19@avenues.org>
2019-05-04 11:01:31 -0400

    k

--- config/database.js
+++ config/database.js
@@ -16,21 +16,8 @@ client
 		for (let row of res.rows) {
 			console.log(JSON.stringify(row));
 		}
+		module.exports = res;
 		// client.end();
 		});
 
 // Option 1: Passing parameters separately
-module.exports = new Sequelize('link_to_articles', process.env.USER, process.env.PASS, {
-	logging: false,
-	host: 'pure-brushlands-63188.herokuapp.com',
-	dialect: 'postgres',
-
-  pool: {
-	  max: 5,
-	  min: 0,
-	  acquire: 30000,
-	  idle: 10000
-  }
-
-  
-});
