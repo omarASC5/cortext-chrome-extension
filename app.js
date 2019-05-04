b4500faffe5afb463cce8fbea24cafe8405df90e
omarASC5 <ocobas19@avenues.org>
2019-05-04 12:28:55 -0400

    sequelize

--- app.js
+++ app.js
@@ -48,6 +48,25 @@ client.query('SELECT table_schema,table_name FROM information_schema.tables;', (
 	client.end();
 });
 
+var sequelize = require('sequelize-heroku').connect(require('sequelize'));
+
+if (sequelize) {
+    sequelize.authenticate().then( function() {
+        var config = sequelize.connectionManager.config;
+        console.log('sequelize-heroku: Connected to '+config.host+' as '+config.username+'.');
+        
+        sequelize.query('SELECT 1+1 as test').then( function(res) {
+            console.log('1+1='+res[0][0].test);
+        });
+        
+    }).catch( function(err) {
+        var config = sequelize.connectionManager.config;
+        console.log('Sequelize: Error connecting '+config.host+' as '+config.user+': '+err);
+    });
+} else {
+    console.log('No environnement variable found.');
+}
+
 
 		// Test DB
 // db.authenticate()
